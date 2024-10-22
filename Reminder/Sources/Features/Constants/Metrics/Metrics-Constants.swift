1429cd235b407f8e71f50153167887c53b42abec
arthur <arthur.rios007@gmail.com>
2024-10-22 20:18:27 -0300

    Aprimorando o projeto Reminder - Ajustando o systemDesign

--- Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
+++ Reminder/Sources/Features/Constants/Metrics/Metrics-Constants.swift
@@ -12,8 +12,10 @@ public struct Metrics {
     public static var huge: CGFloat = 48
     public static var medium: CGFloat = 24
     public static var small: CGFloat = 12
-    public static var little: CGFloat = 8
-    public static var tiny: CGFloat = 4
+    public static var tiny: CGFloat = 8
+    public static var little: CGFloat = 4
     public static var buttonSize: CGFloat = 56
     public static var inputSize: CGFloat = 56
+    public static var profileImageSize: CGFloat = 68
+    public static var backgroundProfileSize: CGFloat = 240
 }
