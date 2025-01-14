13d2f730ab987a5c00443bd512f4c8bd13cd5e38
arthur <arthur.rios007@gmail.com>
2025-01-13 22:06:02 -0300

    Banco e regras de negócio - Observer para botão

--- Reminder/Sources/Features/Scenes/NewReceipt/NewReceiptView.swift
+++ Reminder/Sources/Features/Scenes/NewReceipt/NewReceiptView.swift
@@ -98,6 +98,8 @@ class NewReceiptView: UIView {
         setupTimeInput()
         setupRecurrenceInput()
         setupConstraints()
+        setupObservers()
+        validateInputs()
     }
     
     private func setupConstraints() {
@@ -162,10 +164,27 @@ class NewReceiptView: UIView {
         recurrencePicker.dataSource = self
     }
     
+    private func validateInputs() {
+        let isRemedyFilled = !(remedyInput.textField.text ?? "" ).isEmpty
+        let isTimeFilled = !(timeInput.textField.text ?? "").isEmpty
+        let isRecurrenceFilled = !(recurrenceInput.textField.text ?? "").isEmpty
+        
+        addButton.isEnabled = isRemedyFilled && isTimeFilled && isRecurrenceFilled
+        addButton.backgroundColor = addButton.isEnabled ? Colors.primaryRedBase : Colors.gray500
+    }
+    
+    private func setupObservers() {
+        remedyInput.textField.addTarget(self, action: #selector(inputDidChange), for: .editingChanged)
+        timeInput.textField.addTarget(self, action: #selector(inputDidChange), for: .editingChanged)
+        recurrenceInput.textField.addTarget(self, action: #selector(inputDidChange), for: .editingChanged)
+    }
+    
     @objc private func didSelectRecurrence() {
         let selectedRow = recurrencePicker.selectedRow(inComponent: 0)
         recurrenceInput.textField.text = recurrenceOptions[selectedRow]
         recurrenceInput.textField.resignFirstResponder()
+        
+        validateInputs()
     }
     
     @objc
@@ -174,6 +193,13 @@ class NewReceiptView: UIView {
         formatter.timeStyle = .short
         timeInput.textField.text = formatter.string(from: timePicker.date)
         timeInput.textField.resignFirstResponder()
+        
+        validateInputs()
+    }
+    
+    @objc
+    private func inputDidChange() {
+        validateInputs()
     }
 }
 
